d11b5ddce9e0f82a7e8df1e76b3ef20f9c5aa50f
Jorge <jorgelopezisla@gmail.com>
2021-01-04 17:42:01 -0300

    basic r/w stream created

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 const net = require("net");
+
 const PORT = process.env.PORT || 11211;
+const LINE_FEED = "\r\n";
+let chunk = "";
 
 const server = net.createServer((c) => {
-  //console.log(c);
   c.setEncoding("utf8");
   console.log(`${c.remoteAddress}:${c.remotePort} connected`);
   c.write(`Welcome ${c.remoteAddress}:${c.remotePort} to Memcached Server\r\n`);
-  c.on("data", (chunk) => c.write(chunk));
+  //c.on("data", (chunk) => c.write(chunk));
+  c.on("data", (data) => {
+    const response = readStream(data);
+    if (response) c.write(response);
+  });
   c.on("end", () =>
     console.log(`${c.remoteAddress}:${c.remotePort} disconnected`)
   );
   //c.pipe(c);
 });
 
-server.listen(PORT,"0.0.0.0");
+server.listen(PORT, "0.0.0.0");
 server.on("listening", () => console.log(`Server Memcached active on ${PORT}`));
+
+function readStream(data) {
+  chunk += data;
+  if (chunk.includes(LINE_FEED)) {
+    result = chunk;
+    chunk = "";
+    return result;
+  }
+}
