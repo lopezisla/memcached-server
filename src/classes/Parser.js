aa487c7dce7f64d77f341878cf74a90728858f12
Jorge <jorgelopezisla@gmail.com>
2021-01-07 22:14:51 -0300

    changing parser logic

--- src/classes/Parser.js
+++ src/classes/Parser.js
@@ -8,44 +8,79 @@ const { ERROR, CLIENT_ERROR } = require("../config/serverMessages.js");
 const {
   ALL_COMMANDS,
   STORAGE_COMMANDS,
+  CAS,
   RETRIEVAL_COMMANDS,
 } = require("../config/commands.js");
 
 class Parser {
   constructor() {
-    this.chunk = "";
+    this.dataChunk = "";
+    this.commandToRun = [];
   }
 
-  readStream(data) {
-    this.chunk += data;
+  readStream(chunk) {
+    this.dataChunk += chunk;
     if (this.wasEnterPressed()) {
       if (this.wasBackspacePressed()) {
-        this.chunk = "";
+        this.dataChunk = "";
+        this.commandToRun = [];
         return `${CLIENT_ERROR} control characters are not allowed${LINE_FEED}`;
       } else {
-        const chunkSplit = this.splitChunk();
-        this.chunk = "";
-        console.log(chunkSplit);
-        const isValidCommand = this.parseFullCommand(chunkSplit);
-        if (isValidCommand) {
-          return `${chunkSplit[0]}${LINE_FEED}`;
+        if (!this.commandToRun.length) {
+          const commandChunkSplit = this.splitCommandChunk();
+          this.dataChunk = "";
+          if (commandChunkSplit.length > 1) {
+            const isValidCommand = this.parseFullCommand(commandChunkSplit);
+            console.log(this.commandToRun);
+            if (isValidCommand) {
+              if (RETRIEVAL_COMMANDS.includes(this.commandToRun[0])) {
+                console.log(this.commandToRun);
+                console.log(" get ");
+                console.log("execute get gets" + LINE_FEED);
+                this.commandToRun = [];
+                console.log(this.commandToRun);
+              }
+            } else {
+              return `${ERROR}${LINE_FEED}`;
+            }
+          } else {
+            return `${CLIENT_ERROR} missing parameters${LINE_FEED}`;
+          }
         } else {
-          return `${ERROR}${LINE_FEED}`;
+          console.log("entre a value");
+          const valueChunkSplit = this.dataChunk.replace(LINE_FEED, "");
+
+          console.log(this.commandToRun);
+          console.log(this.dataChunk);
+          console.log(valueChunkSplit);
+          console.log(" storage ");
+          console.log("execute storage" + LINE_FEED);
+          this.commandToRun = [];
+          this.dataChunk = "";
+          console.log(this.commandToRun);
+          console.log(this.dataChunk);
+
+          const result = "execute(comando, valueChunkSplit)" + LINE_FEED;
+          console.log(result);
+          console.log(this.commandToRun.length);
         }
       }
     }
   }
 
   wasEnterPressed() {
-    return this.chunk.includes(LINE_FEED);
+    return this.dataChunk.includes(LINE_FEED);
   }
 
   wasBackspacePressed() {
-    return this.chunk.includes(BACKSPACE);
+    return this.dataChunk.includes(BACKSPACE);
   }
 
-  splitChunk() {
-    return this.chunk.replace(LINE_FEED, "").split(" ");
+  splitCommandChunk() {
+    return this.dataChunk
+      .replace(LINE_FEED, "")
+      .split(" ")
+      .filter((c) => c !== "");
   }
 
   parseFullCommand(fullCommand) {
@@ -60,9 +95,6 @@ class Parser {
     } else {
       return false;
     }
-    // const [command, key, flag, exptime, bytes, noreply] = fullCommand;
-    // console.log([command, key, flag, exptime, bytes, noreply]);
-    // return command;
   }
 
   parseCommand(commandName) {
@@ -73,34 +105,84 @@ class Parser {
     if (STORAGE_COMMANDS.includes(fullCommand[0])) {
       const isValidKey = this.parseKey(fullCommand[1]);
       const isValidFlag = this.parseFlag(fullCommand[2]);
-      if (isValidKey && isValidFlag) {
-        return true;
+      const isValidExptime = this.parseExptime(fullCommand[3]);
+      const isValidBytes = this.parseBytes(fullCommand[4]);
+      if (
+        isValidKey &&
+        isValidFlag[0] &&
+        isValidExptime[0] &&
+        isValidBytes[0]
+      ) {
+        if (fullCommand[0] === CAS) {
+          this.setCommand(
+            fullCommand[0],
+            isValidKey,
+            isValidFlag[1],
+            isValidExptime[1],
+            isValidBytes[1],
+            fullCommand[5]
+          );
+          return true;
+        } else {
+          this.setCommand(
+            fullCommand[0],
+            isValidKey,
+            isValidFlag[1],
+            isValidExptime[1],
+            isValidBytes[1]
+          );
+          return true;
+        }
       } else {
         return false;
       }
     } else {
+        this.setCommand(
+            fullCommand[0],
+            fullCommand[1],
+            fullCommand[2],
+          );
+          return true;
     }
-
-    // let [_, key, flag, exptime, bytes, noreply] = fullCommand;
-    // flag = parseInt(flag);
-    // exptime = parseInt(exptime);
-    // bytes = parseInt(bytes);
-    // noreply = parseInt(noreply);
-    // return [key, flag, exptime, bytes, noreply];
   }
 
   parseKey(key) {
     const validKey = key.match(REGEX);
-    return validKey[0] !== "" ? true : false;
+    return validKey[0] !== "" ? validKey[0] : false;
   }
 
   parseFlag(flag) {
-    const validFlag = parseFloat(flag)
+    const validFlag = parseInt(flag);
     return Number.isInteger(validFlag)
       ? validFlag >= 0 && validFlag <= A_16BIT_UNSIGNED_MAX_VALUE
-        ? true
+        ? [true, validFlag]
         : false
       : false;
+  }
+
+  parseExptime(exptime) {
+    const validExptime = parseInt(exptime);
+    return Number.isInteger(validExptime) ? [true, validExptime] : false;
+  }
+
+  parseBytes(bytes) {
+    const validBytes = parseInt(bytes);
+    return Number.isInteger(validBytes)
+      ? validBytes >= 0
+        ? [true, validBytes]
+        : false
+      : false;
+  }
+
+  setCommand(command, key, flag, exptime, bytes, casUnique) {
+    return (this.commandToRun = [
+      command,
+      key,
+      flag,
+      exptime,
+      bytes,
+      casUnique,
+    ]);
   }
 }
 
