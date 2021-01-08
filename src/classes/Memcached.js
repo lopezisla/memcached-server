38828eb227db59224d7a410cfce36c4d555a6344
Jorge <jorgelopezisla@gmail.com>
2021-01-08 12:33:49 -0300

    singleton pattern created

--- src/classes/Memcached.js
+++ src/classes/Memcached.js
@@ -1,5 +1,10 @@
 class Memcached {
     constructor() {
       this.cache={}
+      if (typeof Memcached.instance === 'object'){
+        return Memcached.instance;
+      }
+      Memcached.instance=this;
+      return this;
     }
   }
